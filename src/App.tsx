7df31fdd00f4435e2ee70946d730f44a3bfb4c98
Matheus Elyasha <matheuselyasha@gmail.com>
2021-06-15 22:41:01 -0300

    configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import { createServer } from 'miragejs';
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
 
 createServer({
   routes() {
@@ -22,11 +24,30 @@ createServer({
   }
 })
 
+Modal.setAppElement('#root');
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+  
       <Dashboard />
+
+
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+          <h2>Cadastrar transação</h2>
+        </Modal>
+
       <GlobalStyle />
     </>
   );
