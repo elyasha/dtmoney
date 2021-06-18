e787cc737bd082e8cfedcab2f94719a24f7111ac
Matheus Elyasha <matheuselyasha@gmail.com>
2021-06-17 21:09:25 -0300

    carregando transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,16 +1,16 @@
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext'
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from './styles'
-import { useContext } from 'react'
 
 
 export default function Summary() {
 
   // Nova API com React Hooks
-  // const data = useContext(TransactionsContext);
-  // console.log(data)
+  const transactions = useContext(TransactionsContext);
+  console.log(transactions)
   return (
     <Container>
       {/* // API antiga */}
