8994a54eb635d851d634cbf14ba778f3c113d017
Matheus Elyasha <matheuselyasha@gmail.com>
2021-06-17 21:38:44 -0300

    finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -18,15 +18,22 @@ export default function NewTransactionModal({ isOpen, onRequestClose }: NewTrans
   const [category, setCategory] = useState('');
   const [amount, setAmount] = useState(0);
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category, 
       type
-    })
+    });
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
+
 
 
 
